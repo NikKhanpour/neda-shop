43d6bb624e96ecd25bb6f25db26c7fdcf1e563c2
nik <nikkhanpour@gmail.com>
2023-08-27 20:55:23 -0700

    add header component & bootstrap icons

--- app.vue
+++ app.vue
@@ -1,3 +1,4 @@
 <template>
-  <NuxtPage/>
+	<LayoutHeader />
+	<NuxtPage />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      htmlAttrs: {
+        dir: 'rtl',
+        lang: 'fa'
+      }
+    }
+  }
 })
